b4c2827852d8bb7d09e41a182766e79f34416571
Kid <kidonng@gmail.com>
2021-05-23 14:02:36 +0800

    Load modules after getting bot info

--- api/index.ts
+++ api/index.ts
@@ -1,9 +1,10 @@
 import { VercelRequest, VercelResponse } from '@vercel/node'
-import { telegraf } from '../src'
+import { telegraf, loadModules } from '../src'
 import { botInfo } from '../src/utils'
 
 export default async ({ body }: VercelRequest, res: VercelResponse) => {
   const user = await telegraf.telegram.getMe()
   Object.assign(botInfo, user)
+  loadModules()
   return telegraf.handleUpdate(body, res)
 }
--- src/index.ts
+++ src/index.ts
@@ -4,14 +4,18 @@ import { botInfo } from './utils'
 const { BOT_TOKEN, IS_VERCEL } = process.env
 
 export const telegraf = new Telegraf(BOT_TOKEN!)
-require('./action')
-require('./release')
+
+export function loadModules() {
+  require('./action')
+  require('./release')
+}
 
 if (!IS_VERCEL) {
   telegraf.telegram.webhookReply = false
 
   telegraf.telegram.getMe().then((user) => {
     Object.assign(botInfo, user)
+    loadModules()
     telegraf.launch()
   })
 }
